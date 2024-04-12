f40714e53b15a92e8b29c6cc3c14c60a65d31880
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-04-12 10:21:40 +0700

    12-Apr-2024

--- src/app/components/end_user/a/fountain-pen/fountain-pen.component.ts
+++ src/app/components/end_user/a/fountain-pen/fountain-pen.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {Meta} from '@angular/platform-browser';
 @Component({
   selector: 'app-fountain-pen',
   templateUrl: './fountain-pen.component.html',
   styleUrls: ['./fountain-pen.component.css']
 })
 export class FountainPenComponent {
-
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      {property: 'og:type', content: 'website'},
+      {property: 'og:title', content: 'Góc chơi bút máy của tôi'},
+      {property: 'og:description', content: 'Đây là bài viết chia sẻ những kiến thức, kinh nghiệm và trải nghiệm của tôi về bút máy và các loại mực dùng cho bút máy.'},
+      {property: 'og:image', content: 'https://mangcaugai1st.github.io/assets/images/20240308_185057.jpg'},
+    ])
+  }
 }
